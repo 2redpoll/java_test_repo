d358878d2b388ead8da74170ba546d73007e1ff0
Vlad Bukreev <2redpoll@gmail.com>
2022-08-05 10:24:14 +0500

    new methods added

--- sandbox/src/main/java/ru/stqa/jtr/sandbox/firstProgram.java
+++ sandbox/src/main/java/ru/stqa/jtr/sandbox/firstProgram.java
@@ -3,7 +3,28 @@ package ru.stqa.jtr.sandbox;
 public class firstProgram {
 
 	public static void main(String[] args) {	
-		System.out.println("Hello, world!");
+		hello("world");
+		hello("user");
+		hello("Vlad");
+
+		double l = 5;
+		System.out.println("Square area with side " + l + " = " + area(l));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Rectangle area with sides " + a + " and " + b +  " = "  + area(a, b));
+	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area(double len) {
+		return len * len;
+	}
+
+	public static double area(double a, double b) {
+		return a * b;
 	}
 	
 }
